04160ee985bf8b059f2aa33108e609974d8db6a0
PurabKumarDas <87687846+PurabKumarDas@users.noreply.github.com>
2021-11-18 17:42:48 +0530

    Checker extended changes

--- Checker/Program.cs
+++ Checker/Program.cs
@@ -1,54 +1,79 @@
 ﻿using System;
 
+
 namespace Checker
 {
-    class Program
+    using CustomExtenion1;
+    public class Program
     {
-        static bool batteryIsOk(float temperature, float soc, float chargeRate)
+        public enum TemperatureUnit
         {
+            Centrigrade, Farenheit
+        }
+        static bool batteryIsOk(float temperature, float soc, float chargeRate, TemperatureUnit unitOfTemperature)
+        {
+
+            if (unitOfTemperature == TemperatureUnit.Farenheit)
+            {
+                temperature = (float)((temperature - 32) / 1.8);
+                chargeRate = (float)((chargeRate - 32) / 1.8);
+                soc = (float)((soc - 32) / 1.8);
+            }
+
             if (temperature < 0 || temperature > 45)
             {
-                Console.WriteLine("Temperature is out of range!");
+                Console.WriteLine(checker.Properties.Resources.TemperatureRange);
                 return false;
             }
+
             else if (soc < 20 || soc > 80)
             {
-                Console.WriteLine("State of Charge is out of range!");
+                Console.WriteLine(checker.Properties.Resources.SocRange);
                 return false;
             }
             else if (chargeRate > 0.8)
             {
-                Console.WriteLine("Charge Rate is out of range!");
+                Console.WriteLine(checker.Properties.Resources.ChargeRateRange);
                 return false;
             }
+
             return true;
         }
 
         static void ExpectStatus(bool expression)
         {
-            if (!expression)
-            {
-                Console.WriteLine("Expected true, but got false");
-                Environment.Exit(1);
-            }
-            else if (expression)
+            if (expression)
             {
                 Console.WriteLine("Expected false, but got true");
-                Environment.Exit(1);
+                //Environment.Exit(1);
+            }
+            else if (!expression)
+            {
+                Console.WriteLine("Expected true, but got false");
+                //Environment.Exit(1);
             }
 
         }
-   
-        static int Main(string[] args)
+
+        public static int Main(string[] args)
         {
-            Func<float,float,float,bool> batteryCheckFunc = new Func<float, float, float, bool>(batteryIsOk);
+            Func<float, float, float, TemperatureUnit, bool> batteryCheckFunc = new Func<float, float, float, TemperatureUnit, bool>(batteryIsOk);
             Action<bool> checkStatusFun = new Action<bool>(ExpectStatus);
 
-            checkStatusFun(batteryCheckFunc(25, 70, 0.7f));
-            checkStatusFun(batteryCheckFunc(50, 85, 0.0f));
-          
+            checkStatusFun(batteryCheckFunc(25, 70, 0.7f,TemperatureUnit.Centrigrade).BatteryWarnings(25, 70, 0.7f,TemperatureUnit.Centrigrade));
+
+            checkStatusFun(batteryCheckFunc(50, 85, 0.0f,TemperatureUnit.Centrigrade).BatteryWarnings(25, 70, 0.7f, TemperatureUnit.Centrigrade));
+
             Console.WriteLine("All ok");
+
             return 0;
         }
+
+
+
     }
+
 }
+
+
+
---
+++ Checker/WarningExtension.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CustomExtenion1
+{
+    public static class WarningExtension
+    {
+        public static bool BatteryWarnings(this bool WarnBatter, float temperature, float soc, float chargeRate, Checker.Program.TemperatureUnit unitOfTemperature)
+        {
+            if (unitOfTemperature == Checker.Program.TemperatureUnit.Farenheit)
+            {
+                temperature = (float)((temperature - 32) / 1.8);
+                chargeRate = (float)((chargeRate - 32) / 1.8);
+                soc = (float)((soc - 32) / 1.8);
+            }
+
+            #region SOC Check
+            if (soc >= 20 && soc <= 20 + 4)
+            {
+
+                Console.WriteLine(checker.Properties.Resources.SOCWarning.Replace("[replace]","SOC"));
+                return false;
+            }
+
+            else if (soc >= 80 - 4 && soc <= 80)
+            {
+
+                Console.WriteLine(checker.Properties.Resources.SOCWarning2.Replace("[replace]", "SOC"));
+                return false;
+            }
+            #endregion
+
+            #region Temperature check
+            if (temperature >= 0 && temperature <= ((5 * 45) / 100))
+            {
+                Console.WriteLine(checker.Properties.Resources.SOCWarning.Replace("[replace]", "Temperature"));
+                return false;
+            }
+            else if (soc >= 80 - 2.25 && soc <= 80)
+            {
+
+                Console.WriteLine(checker.Properties.Resources.SOCWarning2.Replace("[replace]", "Temperature"));
+                return false;
+            }
+            #endregion
+
+            #region ChargeRate check
+            if (chargeRate >= 0 && chargeRate <= 0.8 + 0.04)
+            {
+                Console.WriteLine(checker.Properties.Resources.SOCWarning.Replace("[replace]", "Charge Rate"));
+                return false;
+            }
+            else if (soc >= 0.8 - 0.04 && soc <= 0.8)
+            {
+                Console.WriteLine(checker.Properties.Resources.SOCWarning2.Replace("[replace]", "Charge Rate"));
+                return false;
+            }
+            #endregion
+
+            return true;
+        }
+    }
+}
